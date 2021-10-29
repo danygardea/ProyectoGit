99d041e9f9c7ac3ff7d4bc581d3cc58e411be79d
Daniel Gardea <dany.gardea@gmail.com>
2021-10-28 18:48:11 -0600

    Se agrega la clase de instructor

---
+++ src/main/java/com/esapco/proyectogit/model/instructor.java
@@ -0,0 +1,10 @@
+package com.esapco.proyectogit.model;
+
+import lombok.Data;
+
+@Data
+public class instructor {
+    private String nombre;
+    private String correo;
+    private String direccion;
+}
